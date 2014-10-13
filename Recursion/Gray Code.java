04cef1be82ce1c84856101036bd34e2c397cdfcd
gewhiz <gewhiz07@gmail.com>
2014-10-12 22:27:04 -0400

    https://oj.leetcode.com/problems/gray-code/

---
+++ Recursion/Gray Code.java
@@ -0,0 +1,50 @@
+//The gray code is a binary numeral system where two successive values differ in only one bit.
+
+//Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+//For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+//00 - 0
+//01 - 1
+//11 - 3
+//10 - 2
+//Note:
+//For a given n, a gray code sequence is not uniquely defined.
+
+//For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+
+//For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<Integer>();
+        
+        if(n == 0)
+        {
+            res.add(0);
+            return res;
+        }
+        if(n == 1)
+        {
+            res.add(0);
+            res.add(1);
+            return res;
+        }
+        List<Integer> pre = grayCode(n-1);
+        res.addAll(pre);
+        for(int i = pre.size()-1;i >=0; i--)
+        {
+            int num = pre.get(i);
+            String k =Integer.toBinaryString(num);
+            while(k.length() < n-1)
+            {
+            	k="0"+k;
+            }
+		    k = "1" + k;
+		    int newNum = Integer.parseInt(k, 2);
+		    res.add(newNum);
+        }
+            
+        return res;
+    }
+}
